127d4e4c20f046ca7b76f7f724a5fbc6a18f8e0f
juanbruno99 <juan.marcos.bruno@gmail.com>
2021-04-01 12:32:09 +0100

    Added Embedded DB functionality

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Spark Databases Ingestion Example Project
 
-Project demonstrates usage of files loading functionality, for RDBMS such as PostgresSQL, within the *Spark* API.
+Project demonstrates usage of loading functionality, for RDBMS such as PostgresSQL, within the *Spark* API.
 
 To run execute:
 
--- src/main/java/com/jmb/DBIngestion.java
+++ src/main/java/com/jmb/DBIngestion.java
@@ -1,5 +1,6 @@
 package com.jmb;
 
+import com.jmb.datasource.DerbyDBManager;
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.*;
 import org.slf4j.Logger;
@@ -7,6 +8,9 @@ import org.slf4j.LoggerFactory;
 
 
 public class DBIngestion {
+
+    public static final String EMBEDDED_DRIVER_STRING = "org.apache.derby.jdbc.EmbeddedDriver";
+
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DBIngestion.class);
 
@@ -20,13 +24,18 @@ public class DBIngestion {
 
     private void init() throws Exception {
 
-        SparkConf appConfig = new SparkConf().set("spark.testing.memory", "900000000");
+        DerbyDBManager dbManager = new DerbyDBManager();
+
+        //Start the embedded Derby DB (in memory) - Runs also INSERT queries
+        dbManager.startDB();
 
         //Create the Spark Session
         SparkSession session = SparkSession.builder()
-                .appName("SparkFileFormats")
-                .config(appConfig)
+                .appName("SparkDBIngestion")
                 .master("local").getOrCreate();
+
+        //Stop the embedded Derby DB (in memory) - Runs also DELETE TABLE query
+        dbManager.stopDB();
 
     }
 
